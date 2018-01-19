e1eb704d95deb96d4215f2e05e26c36b42ccb3c4
EliScherrer <escherre@purdue.edu>
2018-01-19 14:53:58 -0500

    added popular and started top rated

---
+++ flix/Views/PosterCell.swift
@@ -0,0 +1,16 @@
+//
+//  PosterCell.swift
+//  flix
+//
+//  Created by Eli Scherrer on 1/19/18.
+//  Copyright © 2018 Eli Scherrer. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    
+}
